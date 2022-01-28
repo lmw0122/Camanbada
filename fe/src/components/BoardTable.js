4bbfa3472f7ff4ddc6c366c963934b2597ad1949
이다예 <helen121027@gmail.com>
2022-01-28 10:15:20 +0900

    220128 오전 - DY

---
+++ fe/src/components/BoardTable.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Pagination from '@mui/material/Pagination';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Container, CssBaseline, dividerClasses, Grid, Typography } from '@mui/material';
+
+const theme = createTheme();
+
+function createData(num, tag, title, writer, created_at) {
+  return { num, tag, title, writer, created_at };
+}
+
+const rows = [
+  createData(1, '나눔', '고기 나눔합니다!', 'LEE', '2022-01-27'),
+  createData(2, '나눔', '고기 나눔합니다!', 'LEE', '2022-01-27'),
+  createData(3, '나눔', '고기 나눔합니다!', 'LEE', '2022-01-27'),
+  createData(4, '나눔', '고기 나눔합니다!', 'LEE', '2022-01-27'),
+  createData(5, '나눔', '고기 나눔합니다!', 'LEE', '2022-01-27'),
+];
+
+export default function BoardTable() {
+  return (
+    <ThemeProvider theme={theme} >
+      <Grid container sx={{ height : '100vh', m: 4}}>
+        <CssBaseline />
+        <TableContainer component={Paper} maxWidth="sm" align="center">
+          <Table sx={{ m : 2, width: 700 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell align="center">말머리</TableCell>
+                <TableCell align="center">제목</TableCell>
+                <TableCell align="center">작성자</TableCell>
+                <TableCell align="center">작성날짜</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.num}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.num}
+                  </TableCell>
+                  <TableCell align="center">{row.tag}</TableCell>
+                  <TableCell align="center">{row.title}</TableCell>
+                  <TableCell align="center">{row.writer}</TableCell>
+                  <TableCell align="center">{row.created_at}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <Pagination count={10} showFirstButton showLastButton sx={{ m: 2 }} />  
+        </TableContainer>
+      </Grid>
+    </ThemeProvider>
+  )
+}
--- fe/src/routes/Community.js
+++ fe/src/routes/Community.js
@@ -1,73 +1,35 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Pagination from '@mui/material/Pagination';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, Grid, Typography } from '@mui/material';
+import NavBar from '../components/NavBar'
+import SelectVariantsBoard from '../components/SelectVariantsBoard';
+import Button from '@mui/material/Button';
+import { Container, CssBaseline, Grid, Typography } from '@mui/material';
+import { Link } from "react-router-dom";
+import BoardTable from '../components/BoardTable';
 
-const theme = createTheme();
-
-function createData(num, tag, title, writer, created_at) {
-  return { num, tag, title, writer, created_at };
-}
-
-const rows = [
-  createData(1, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-  createData(2, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-  createData(3, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-  createData(4, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-  createData(5, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-  createData(6, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-  createData(7, '나눔', '고기 나눔합니다!!', 'lee', '2020-01-26'),
-];
-
-export default function BasicTable() {
+function Community() {
   return (
-    <ThemeProvider theme={theme} >
-      <Grid container sx={{ height : '100vh' }}>
+    <div>
+      <NavBar />    
+      <Grid container sx={{ height : '100vh', m: 4}}>
         <CssBaseline />
-        <Grid item xs={12}>
-          <img src="img/camping.jpg" sx={{ m: 2 }} alt="community"/>
-          <Typography component="h1" variant="h3" align="center" sx={{m: 2}}> 
-            CAM:NABADA Community
-          </Typography>
-          <TableContainer component={Paper} maxWidth="sm">
-            <Table sx={{ m : 2 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>번호</TableCell>
-                  <TableCell align="center">말머리</TableCell>
-                  <TableCell align="center">제목</TableCell>
-                  <TableCell align="center">작성자</TableCell>
-                  <TableCell align="center">작성날짜</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.num}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.num}
-                    </TableCell>
-                    <TableCell align="center">{row.tag}</TableCell>
-                    <TableCell align="center">{row.title}</TableCell>
-                    <TableCell align="center">{row.writer}</TableCell>
-                    <TableCell align="center">{row.created_at}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Pagination count={10} showFirstButton showLastButton />
+        <Grid item xs={12} align="center">
+          <Container sx={{mt : 10, mb : 2}}>
+            <img src="img/camping.jpg" sx={{ mt: 20 }} alt="camping"/>
+          </Container>
+          <div>
+            <Typography component="h1" variant="h4" align="center" sx={{m: 2}}> 
+              CAM:NABADA 커뮤니티
+            </Typography>
+            <Link to={'/create'} underline="none">
+              <Button variant="contained">게시글 작성</Button>
+            </Link>
+          </div>
+          <SelectVariantsBoard />
         </Grid>
+      <BoardTable />  
       </Grid>
-    </ThemeProvider>
-  );
+    </div>
+  )
 }
+
+export default Community;
