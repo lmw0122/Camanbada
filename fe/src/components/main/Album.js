9601b572acc76e454c30b5deaebdf5f80cf21640
김나영 <ny3021@gmail.con>
2022-02-12 04:10:20 +0900

    메인페이지

--- fe/src/components/main/Album.js
+++ fe/src/components/main/Album.js
@@ -7,12 +7,42 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Axios from "axios";
+import CampingImage from '../camping/CampingImage';
 
 const cards = [1, 2, 3];
 
 const theme = createTheme();
 
 export default function Album() {
+
+  const [basics, setBasics] = React.useState([]);
+
+  React.useEffect(() => {
+    Axios.get(`http://i6c109.p.ssafy.io:8092/camp/basic/list`)
+      .then(res => setBasics(res.data))
+  }, []);
+
+  const topCamps = [];
+
+  // 전체 캠핑장 좋아요 순 정렬
+  // componentWillMount() {
+    
+  // }
+  
+  for (var i=0; i<basics.length; i++) {
+    basics.sort(function(a,b) {
+      return b.likes - a.likes;
+    })
+  }
+
+  // 상위 3개만 뽑아오기
+  for (var i=0; i<3; i++) {
+    topCamps.push(basics[i])
+  }
+  
+  console.log(topCamps)
+  
   return (
     <ThemeProvider theme={theme}>    
       <CssBaseline />
@@ -26,15 +56,6 @@ export default function Album() {
           }}
         >
           <Container maxWidth="lg">
-            {/* <Stack
-              sx={{ 
-                mb: 5, 
-              }}
-              direction="row"
-              justifyContent="center"
-            >
-              <img src={require("../img/logo.png")} alt="logo"></img>
-            </Stack> */}
             <Typography
               sx={{
                 fontWeight: 'bold',
@@ -46,40 +67,22 @@ export default function Album() {
             >
               현재 인기 캠핑장
             </Typography>
-            {/* <Typography variant="h6" align="center" color="text.secondary" paragraph>
-              좋아요 수가 많은 순서대로 캠핑장을 추천해준다.
-            </Typography> */}
           </Container>
         </Box>
         <Container sx={{ py: 0 }} maxWidth="lg">
-          {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+            {topCamps.map((item) => (
+              <Grid item key={item.campId} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
                   align="center"
                 >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      // pt: '56.25%',
-                      pt: '0%',
-                    }}
-                    image="https://images.unsplash.com/photo-1641157141085-8454fbc33f3c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY0MzExNTc1NQ&ixlib=rb-1.2.1&q=80&w=1080"
-                    alt="CampingImage"
-                  />
+                  <CampingImage basics={ item }></CampingImage>
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      $캠핑장 이름
+                      {item.facltNm}
                     </Typography>
-                    {/* <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography> */}
                   </CardContent>
-                  {/* 캠핑장 상세 정보 링크 걸기 */}
                   <Link to={'/campingdetail'} >
                     <CardActions >
                       <Button size="small" >상세정보</Button>
